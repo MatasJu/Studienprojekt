531ec2149656f47abb26470f8d6a5cd7a5003f97
Matas Jurevicius <s-mjurev@haw-landshut.de>
2017-11-07 00:03:09 +0100

    Initial Commit

---
+++ app/src/main/java/de/haw_landshut/studienprojekt/MainActivity.java
@@ -0,0 +1,13 @@
+package de.haw_landshut.studienprojekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
