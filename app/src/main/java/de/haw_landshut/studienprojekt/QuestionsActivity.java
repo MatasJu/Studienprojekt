69735906183253c62da55daad4e6a1490a5fa1a7
MatasJu <matasju@gmail.com>
2018-04-02 19:26:40 +0200

    add SimulationSettings and QuestionsActivity

---
+++ app/src/main/java/de/haw_landshut/studienprojekt/QuestionsActivity.java
@@ -0,0 +1,175 @@
+package de.haw_landshut.studienprojekt;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+public class QuestionsActivity extends AppCompatActivity {
+    //This constant uses the name of the class itself as the tag.
+    private static final String TAG = QuestionsActivity.class.getSimpleName();
+
+    //
+    TextToSpeech t1;
+
+    TextView ansTxt;
+
+
+    //------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        ansTxt =(TextView) findViewById(R.id.answer);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if(status == TextToSpeech.SUCCESS) {
+
+                    Log.d(TAG, "onInit: we got here");
+                    t1.setLanguage(Locale.GERMANY);
+                    t1.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+
+                        @Override
+                        public void onDone(String utteranceId) {
+                            Log.d(TAG, "TTS start");
+                            displaySpeechRecognizer();
+                        }
+
+                        @Override
+                        public void onError(String utteranceId) {
+                            Log.d(TAG, "TTS erroor");
+                        }
+
+                        @Override
+                        public void onStart(String utteranceId) {
+                            Log.d(TAG, "TTS finished");
+                        }
+                    });
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "UniqueID");
+
+                    t1.speak(getString(R.string.what_day_question), TextToSpeech.QUEUE_ADD, map);
+
+                }
+            }
+        });
+    }
+
+    private static final int SPEECH_WHAT_DAY_CODE = 7;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.GERMAN.toString());
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,false);
+        intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,getString(R.string.what_day_question));
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,2);
+
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_WHAT_DAY_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SPEECH_WHAT_DAY_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            String spokenText = results.get(0);
+            Log.d(TAG, "onActivityResult() called with: requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "], results.toString()"+results.toString());
+
+            // Do something with spokenText
+
+            ansTxt.setText(spokenText);
+            checkDayAnswer(spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private void checkDayAnswer(String result){
+
+
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+        boolean tag = false;
+
+        switch (day) {
+            case Calendar.SUNDAY:
+
+                if(result.toLowerCase().contains("sontag"))
+                    tag = true;
+
+                break;
+
+            case Calendar.MONDAY:
+
+                if(result.toLowerCase().contains("montag"))
+                    tag = true;
+
+                break;
+
+            case Calendar.TUESDAY:
+
+                if(result.toLowerCase().contains("dienstag"))
+                    tag = true;
+
+                break;
+
+            case Calendar.WEDNESDAY:
+
+                if(result.toLowerCase().contains("mittwoch"))
+                    tag = true;
+
+                break;
+            case Calendar.THURSDAY:
+
+                if(result.toLowerCase().contains("donnerstag"))
+                    tag = true;
+
+                break;
+
+            case Calendar.FRIDAY:
+
+                if(result.toLowerCase().contains("freitag"))
+                    tag = true;
+
+                break;
+
+            case Calendar.SATURDAY:
+
+                if(result.toLowerCase().contains("samstag"))
+                    tag = true;
+
+                break;
+        }
+
+
+        //tag the person.
+        ansTxt.setText(tag?R.string.tag_color_delayed:R.string.tag_color_immediate);
+
+        this.getWindow().getDecorView().setBackgroundColor(tag? Color.YELLOW:Color.RED);
+
+    }
+
+
+
+}
--- app/src/main/java/de/haw_landshut/studienprojekt/SimulationSettings.java
+++ app/src/main/java/de/haw_landshut/studienprojekt/SimulationSettings.java
@@ -1,16 +1,140 @@
 package de.haw_landshut.studienprojekt;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SimulationSettings extends AppCompatActivity {
 
     //This constant uses the name of the class itself as the tag.
     private static final String LOG_TAG = SimulationSettings.class.getSimpleName();
 
+    //
+    private SeekBar HRseekbar;
+    private SeekBar RRseekbar;
+    private TextView HRtag;
+    private TextView RRtag;
+    private TextView HRTextView;
+    private TextView RRTextView;
+    private Button continueBtn;
+
+    private final int RRHIGH =120;
+    private final int RRNORM =51;
+
+    private final int HRHIGH = 20;
+    private final int HRNORM = 12;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulation_settings);
+
+        //init
+        HRseekbar = (SeekBar) findViewById(R.id.HRseekBar);
+        RRseekbar = (SeekBar) findViewById(R.id.RRseekBar);
+
+        HRTextView = (TextView) findViewById(R.id.HRTextView);
+        RRTextView = (TextView) findViewById(R.id.RRTextView);
+
+        HRtag = (TextView) findViewById(R.id.HRtag);
+        RRtag = (TextView) findViewById(R.id.RRtag);
+
+        continueBtn = (Button) findViewById(R.id.continueBtn);
+
+        updateStatusValues();
+
+        //Listeners
+        HRseekbar.setOnSeekBarChangeListener(seekBarListener);
+        RRseekbar.setOnSeekBarChangeListener(seekBarListener);
+
+    }
+
+    // The seek bar notification listener https://developer.android.com/reference/android/widget/SeekBar.html
+    private SeekBar.OnSeekBarChangeListener seekBarListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            updateStatusValues();
+//            switch(seekBar.getId())
+//            {
+//                case R.id.HRseekBar:
+//                    HRTextView.setText(Integer.toString(i));
+//                    return;
+//                case R.id.RRseekBar:
+//                    RRTextView.setText(Integer.toString(i));
+//                    return;
+//
+//            }
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            //update current progress indicator.
+
+        }
+
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+    };
+
+    private void updateStatusValues(){
+        HRTextView.setText(Integer.toString(HRseekbar.getProgress()));
+        RRTextView.setText(Integer.toString(RRseekbar.getProgress()));
+
+        //HR TAG COLORS AND TEXT
+        if((HRseekbar.getProgress()>=HRHIGH)){
+            HRtag.setBackgroundColor(Color.RED);
+            HRtag.setText("HIGH");
+        }
+        if((HRseekbar.getProgress()>=HRNORM) && HRseekbar.getProgress()<HRHIGH){
+            HRtag.setBackgroundColor(Color.GREEN);
+            HRtag.setText("NORMAL");
+        }
+
+        if(HRseekbar.getProgress()<HRNORM){
+            HRtag.setBackgroundColor(Color.RED);
+            HRtag.setText("LOW");
+        }
+
+
+        //RR TAG COLORS AND TEXT
+        if((RRseekbar.getProgress()>=RRHIGH)){
+            RRtag.setBackgroundColor(Color.RED);
+            RRtag.setText("HIGH");
+        }
+        if((RRseekbar.getProgress()>=RRNORM) && RRseekbar.getProgress()<RRHIGH){
+            RRtag.setBackgroundColor(Color.GREEN);
+            RRtag.setText("NORMAL");
+        }
+
+        if(RRseekbar.getProgress()<RRNORM){
+            RRtag.setBackgroundColor(Color.RED);
+            RRtag.setText("LOW");
+        }
+
+    }
+
+
+    public void onClickBtn(View view) {
+        if(view.getId()== continueBtn.getId() )
+            {
+                Log.d(LOG_TAG, "continueBtn Clicked!");
+                Intent intent = new Intent(this, QuestionsActivity.class);
+                startActivity(intent);
+            }
+
+
     }
 }
