59700dca0383f0588c3e7a6c70dcfe225a20fae9
MatasJu <s-mjurev@haw-landshut.de>
2018-05-14 17:35:10 +0200

    code clean-up

--- app/src/main/java/de/haw_landshut/studienprojekt/QuestionsActivity.java
+++ app/src/main/java/de/haw_landshut/studienprojekt/QuestionsActivity.java
@@ -1,7 +1,6 @@
 package de.haw_landshut.studienprojekt;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
@@ -26,8 +25,11 @@ import static de.haw_landshut.studienprojekt.BuildConfig.DEBUG;
 
 /**
  * An Activity for asking Questions and taking answers with TTS and STT.
+ * <p>
  * It uses google intents to translate questions to speech and read them,
  * than takes an answer through speech and looks for answer keywords in the returned String.
+ * <p>
+ * TODO: Separate TTS and STT into their own classes could be a good idea?
  */
 public class QuestionsActivity extends AppCompatActivity {
     //This constant uses the name of the class itself as the tag.
@@ -35,12 +37,15 @@ public class QuestionsActivity extends AppCompatActivity {
 
 
 
-    //TTS vars
+    /*===============TTS VARIABLES=================*/
+
+    private boolean isAskingQuestion = false;
 
     //name for the TTS engine we want to use, in this case we make sure that the user has the default android TTS, and init with it.
     private final String googleTTSPackage = "com.google.android.tts";
 
-    /**Listener for TTS to know when it has been initialised.
+    /**
+     * Listener for TTS to know when it has been initialised.
      *
      * @return the TextToSpeech.OnInitListener.
      */
@@ -50,39 +55,77 @@ public class QuestionsActivity extends AppCompatActivity {
                 public void onInit(int status) {
                     if (status == TextToSpeech.SUCCESS) {
                         setupTTS();
-                    }else {
+                    } else {
                         handleTTSinitError();
                     }
 
                 }
             };
 
-    /**This function is run after TTS is initialised. We need TTS to start asking questions, so it also starts our questioning after setting up some settings.
-     * We use TTS to start questioning when every TTS setup is done by speaking out empty string and catching it with utteranceProgressListener.
-     * TODO: set language should be according to the localisation.
+    /**
+     * TTS progress listener to know when TTS has finished speaking and other states.
+     *
+     * @return the UtteranceProgressListener;
      */
-    private void setupTTS() {
-        textToSpeech.setOnUtteranceProgressListener(utteranceProgressListener);
-        textToSpeech.setLanguage(Locale.GERMANY);
+    private UtteranceProgressListener utteranceProgressListener =
+            new UtteranceProgressListener() {
 
-        //this will start the questioning.
-        addToTTSandFlush(" ","TTSdone");
+                @Override
+                public void onDone(String utteranceId) {
+                    isAskingQuestion = false;
+                    TTSonDoneHandler(utteranceId);
+                }
 
-    }
+                @Override
+                public void onError(String utteranceId) {
+                    isAskingQuestion = false;
+                    Log.d(TAG, "TTS error");
+                }
+
+                @Override
+                public void onStart(String utteranceId) {
+                    isAskingQuestion = true;
+                    Log.d(TAG, "TTS start");
+                }
+            };
+
 
     private final String TTS_PAUSE = "... ";
 
-    //For Questioning
+    /*===============END TTS VARIABLES=================*/
+
+
+
+
+    /*===============START STT VARIABLES=====================*/
+
+    /**
+     * Code for recognising callback from speech recogniser.
+     */
+    private static final int SPEECH_WHAT_DAY_CODE = 7;
+
+
+    /*===============END STT VARIABLES=================*/
+
+
+
+
+
+    /*==============START QUESTIONNAIRE VARIABLES============*/
 
     private final int TOTAL_RANDOM_WORD_COUNT = 499;
     private final int AMOUNT_RANDOM_WORDS = 3;
     private final String RANDOM_WORD_ID = "random_word";
-    private TextToSpeech textToSpeech;
 
-    private boolean isAskingQuestion = false;
+
+    private int questionNr = -2;
+    ArrayList<String> questionList;
+
     private int currentQuestionID;
 
-    private ArrayList<String> makeQuestionList = new ArrayList<>();
+    private String[] randomWordsList;
+
+    private final ArrayList<String> makeQuestionList = new ArrayList<>();
 
     {
 
@@ -95,34 +138,31 @@ public class QuestionsActivity extends AppCompatActivity {
     }
 
 
-    //For Testing
-    private TextView testSpeechRecResult;
-    ArrayList<String> questionList;
-    private int questionNr = -2;
+    /*==============END QUESTIONNAIRE VARIABLES============*/
 
-    private String[] randomWordsList;
+
+    /*===========START ACTIVITY VARIABLES===========*/
     private Bundle settingsBundle;
+    private TextToSpeech TTSEngine;
 
-    //------
-    /**
-     * Code for recognising callback from speech recogniser.
-     */
-    private static final int SPEECH_WHAT_DAY_CODE = 7;
+    private TextView testSpeechRecResult;
+    //end for testing
+
+    /*===========END ACTIVITY VARIABLES===========*/
 
 
+    /*===========ACTIVITY FUNCTIONS===========*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
         //init Lists
-
-
         randomWordsList = selectRandomWords(); //String[] for words to remember.    //init Lists
 
-        //pass along the settings for the simulated evaluation.
+        //save simulated settings from the prev activity. could change in the future how its handled.
+        //TODO:is this the best way to pass variables between activities?
         settingsBundle = new Bundle();
-
         settingsBundle.putAll(getIntent().getExtras());
 
     }
@@ -142,52 +182,30 @@ public class QuestionsActivity extends AppCompatActivity {
         //--TestThings®
 
 
-
         currentQuestionID = 0;
 
         //try to start TTS, we are hoping the default google TTS is in place.
         try {
-            textToSpeech = new TextToSpeech(getApplicationContext(), TTSonInitListener, googleTTSPackage);
-        }catch (Exception e){
-            Log.e(TAG, "onStart: ",e );
+            TTSEngine = new TextToSpeech(getApplicationContext(), TTSonInitListener, googleTTSPackage);
+        } catch (Exception e) {
+            Log.e(TAG, "onStart: ", e);
         }
 
     }
 
-    /**TTS progress listener to know when TTS has finished speaking and other states.
-     *
-     * @return the UtteranceProgressListener;
-     */
-    private UtteranceProgressListener utteranceProgressListener =
-         new UtteranceProgressListener() {
+    /*===========END ACTIVITY FUNCTIONS ===========*/
 
-            @Override
-            public void onDone(String utteranceId) {
-                isAskingQuestion = false;
-                TTSonDoneHandler(utteranceId);
-            }
+    /*===========QUESTIONNAIRE FUNCTIONS===========*/
 
-            @Override
-            public void onError(String utteranceId) {
-                isAskingQuestion = false;
-                Log.d(TAG, "TTS error");
-            }
-
-            @Override
-            public void onStart(String utteranceId) {
-                isAskingQuestion = true;
-                Log.d(TAG, "TTS start");
-            }
-        };
-
-    /** this controls the whole Questioning spiel.
+    /**
+     * this controls the whole Questioning spiel.
      * depends on the currentQuestionID to ask the next question.
-     *@see <code>currentQuestionID</code>
-     * @return true wenn all questions have been asked.
+     *
+     * @return true if all questions have been asked.
      */
-    private boolean questionsFlow(){
+    private boolean questionsFlow() {
 
-        switch (currentQuestionID){
+        switch (currentQuestionID) {
 
             case 0: // Ask to Remember the Words
                 isAskingQuestion = true;
@@ -195,16 +213,16 @@ public class QuestionsActivity extends AppCompatActivity {
                 break;
 
 
-
         }
 
         return false;
     }
 
-    private boolean askQuestion(String question){
+    private boolean askQuestion(String question) {
 
         return false;
     }
+
     /**
      * Function to ask the user to remember words.
      */
@@ -215,100 +233,12 @@ public class QuestionsActivity extends AppCompatActivity {
             temp = temp.concat(randomWordsList[i] + TTS_PAUSE);
         }
 
-        addToTTSandFlush(temp,"askToRemember");
-    }
-
-
-
-
-
-
-    /**Adds strings to the end of TTS queue to be spoken out.
-     *
-     * @param text String to speak out.
-     * @param uniqueID unique id that will be used to call out response handling function.
-     */
-
-    private void addToTTSQueue(String text, String uniqueID){
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, uniqueID);
-        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, map);
-
-    }
-    private void addToTTSandFlush(String text, String uniqueID){
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, uniqueID);
-        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, map);
-
-    }
-
-    /**Handles the call from TTS when it is done speaking.
-     *
-     * @param utteranceId TTS returns the calls "unique" id.
-     */
-
-    private void TTSonDoneHandler(String utteranceId) {
-        if(utteranceId.equals("TTSdone")){
-            questionsFlow();
-        }
-
-        testSpeechRecResult.append("Just Asked the words.");
-    }
-
-
-    /**
-     * Creates and displays an intent to write speech to text.
-     * TODO: Localisation, go over settings and choose the best ones, maybe allow the caller to choose them. Modularise this function
-     */
-    private void displaySpeechRecognizer() {
-
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.GERMAN.toString());
-        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false);
-        //Offline recognistion will work oply obove api lvl 23, also has to be downloaded outside the app in the language settings.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
-        }
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.what_day_question));
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 2);
-
-        // Start the activity, the intent will be populated with the speech text
-        startActivityForResult(intent, SPEECH_WHAT_DAY_CODE);
-
-
-    }
-
-    /**
-     * Callback function for STT.
-     *
-     * @param requestCode is the code of the STT intent.
-     * @param resultCode  STT passes the code of success/failure.
-     * @param data        data from the STT.
-     */
-    // This callback is invoked when the Speech Recognizer returns.
-    // This is where you process the intent and extract the speech text from the intent.
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult: ");
-        if (requestCode == SPEECH_WHAT_DAY_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-            String spokenText = results.get(0);
-            Log.d(TAG, "onActivityResult() called with: requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "], results.toString()" + results.toString());
-
-            // Do something with spokenText
-
-            testSpeechRecResult.append("Spoken Text: "+ spokenText);
-            checkAnswer(spokenText);
-
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+        addToTTSandFlush(temp, "askToRemember");
     }
 
     private void checkAnswer(String result) {
-        switch (questionNr) {
-            case -1:
+        switch (currentQuestionID) {
+            case 0:
 
                 //askQuestion();
                 break;
@@ -332,9 +262,126 @@ public class QuestionsActivity extends AppCompatActivity {
 
     }
 
+
+    /*===========END QUESTIONNAIRE METHODS===========*/
+
+    /*===========TTS METHODS=================*/
+
+    /**
+     * This function is run after TTS is initialised. We need TTS to start asking questions, so it also starts our questioning after setting up some settings.
+     * We use TTS to start questioning when every TTS setup is done by speaking out empty string and catching it with utteranceProgressListener.
+     * TODO: set language should be according to the localisation.
+     */
+    private void setupTTS() {
+        TTSEngine.setOnUtteranceProgressListener(utteranceProgressListener);
+        TTSEngine.setLanguage(Locale.GERMANY);
+
+        //this will start the questioning.
+        addToTTSandFlush(" ", "TTSdone");
+
+    }
+
+    /**
+     * Adds strings to the end of TTS queue to be spoken out.
+     *
+     * @param text     String to speak out.
+     * @param uniqueID unique id that will be used to call out response handling function.
+     */
+
+    private void addToTTSQueue(String text, String uniqueID) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, uniqueID);
+        TTSEngine.speak(text, TextToSpeech.QUEUE_ADD, map);
+
+    }
+
+    private void addToTTSandFlush(String text, String uniqueID) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, uniqueID);
+        TTSEngine.speak(text, TextToSpeech.QUEUE_ADD, map);
+
+    }
+
+    /**
+     * Handles the call from TTS when it is done speaking.
+     *
+     * @param utteranceId TTS returns the calls "unique" id.
+     */
+
+    private void TTSonDoneHandler(String utteranceId) {
+        if (utteranceId.equals("TTSdone")) {
+            questionsFlow();
+        }
+
+        testSpeechRecResult.append("Just Asked the words.");
+    }
+
+    /*================END TTS METHODS==================*/
+
+    /*==================STT METHODS======================*/
+
+    /**
+     * Creates and displays an intent to write speech to text.
+     * <p>
+     * TODO: Localisation, go over settings and choose the best ones, maybe allow the caller to choose them. Modularise this function
+     */
+    private void displaySpeechRecognizer() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.GERMAN.toString());
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false);
+
+        //Offline recognistion will work only above api lvl 23, also has to be downloaded outside the app in the language settings.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
+        }
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.what_day_question));
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 2);
+
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_WHAT_DAY_CODE);
+
+
+    }
+
+    /**
+     * Callback function for STT.
+     *
+     * @param requestCode is the code of the STT intent.
+     * @param resultCode  STT passes the code of success/failure.
+     * @param data        data from the STT.
+     */
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SPEECH_WHAT_DAY_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            //the result at place 0 is the best guess at this time....
+            String spokenText = results.get(0);
+
+
+            if (DEBUG) {
+                testSpeechRecResult.append("Spoken Text: " + spokenText);
+            }
+            //Call a handler with the text.
+            checkAnswer(spokenText);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    /*============== END STT functions====================*/
+
+
     //---------------------DONE Functions, low priority.
 
-    /**Checks the result string for 3 words mentioned before. Returns an int of correct answers.
+    /**
+     * Checks the result string for 3 words mentioned before. Returns an int of correct answers.
      *
      * @param result string to be checked for 3 words.
      * @return amount of correct answers.
@@ -344,13 +391,12 @@ public class QuestionsActivity extends AppCompatActivity {
 
         int correct = 0;
 
-
-
         for (String aRandomWordsList : randomWordsList) {
             testSpeechRecResult.append("\n" + aRandomWordsList);
             if (result.toLowerCase().contains(aRandomWordsList.toLowerCase()))
                 correct++;
         }
+
         if (DEBUG) {
             Log.d(TAG, " checkWordsAnswer called with: result = [" + result + "]");
             testSpeechRecResult.append("\nTTS Answer:" + result);
@@ -361,10 +407,8 @@ public class QuestionsActivity extends AppCompatActivity {
     }
 
 
-
-
-    /** Checks if the String has the current month in it.
-     *
+    /**
+     * Checks if the String has the current month in it.
      *
      * @param result string to be checked.
      */
@@ -381,16 +425,13 @@ public class QuestionsActivity extends AppCompatActivity {
             testSpeechRecResult.append("\nAnswer:" + result);
             testSpeechRecResult.append("\nAnswer contains Month:" + result.contains(currentMonth));
         }
-        boolean tag = false;
 
-        if (result.contains(currentMonth))
-            tag = true;
-        else
-            tag = false;
+        return result.contains(currentMonth);
 
     }
 
-    /**Checks if the string has current year in it.
+    /**
+     * Checks if the string has current year in it.
      *
      * @param result
      */
@@ -410,13 +451,12 @@ public class QuestionsActivity extends AppCompatActivity {
 
         return result.contains(currentYear);
 
-
-
     }
 
 
-    /**Check the string for mentioning of the day
-     *
+    /**
+     * Check the string for mentioning of the day
+     * <p>
      * TODO : Localisation.
      */
     private boolean checkDayAnswer(String result) {
@@ -435,7 +475,6 @@ public class QuestionsActivity extends AppCompatActivity {
 
         return result.contains(currentDay);
 
-
     }
 
 
@@ -450,7 +489,7 @@ public class QuestionsActivity extends AppCompatActivity {
 
         for (int i = 0; i < r.length; i++) {
             r[i] = getString(getResources().getIdentifier(
-                    RANDOM_WORD_ID +(new Random().nextInt(TOTAL_RANDOM_WORD_COUNT) + 1),
+                    RANDOM_WORD_ID + (new Random().nextInt(TOTAL_RANDOM_WORD_COUNT) + 1),
                     "string", getPackageName())
             );
 
@@ -460,8 +499,8 @@ public class QuestionsActivity extends AppCompatActivity {
     }
 
 
-    /**TODO:A handler for when a TTS engine fails to initialise.
-     *
+    /**
+     * TODO:A handler for when a TTS engine fails to initialise.
      */
     private void handleTTSinitError() {
     }
